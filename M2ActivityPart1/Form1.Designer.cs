72f7f35690d662353d82956acfcddb6b6032d364
Kyle Zimmerman <kaz0010@auburn.com>
2022-03-13 14:56:29 -0700

    updated GUI to inlcude clear and calculate button. Also added fields to display CalcStayCharges, and CalcMiscCharges.

--- M2ActivityPart1/Form1.Designer.cs
+++ M2ActivityPart1/Form1.Designer.cs
@@ -41,39 +41,45 @@
             this.label6 = new System.Windows.Forms.Label();
             this.totalLabel = new System.Windows.Forms.Label();
             this.totalTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.CalculateBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // numDaysTextBox
             // 
-            this.numDaysTextBox.Location = new System.Drawing.Point(176, 45);
+            this.numDaysTextBox.Location = new System.Drawing.Point(176, 25);
             this.numDaysTextBox.Name = "numDaysTextBox";
             this.numDaysTextBox.Size = new System.Drawing.Size(100, 20);
             this.numDaysTextBox.TabIndex = 0;
             // 
             // medicationTextBox
             // 
-            this.medicationTextBox.Location = new System.Drawing.Point(176, 71);
+            this.medicationTextBox.Location = new System.Drawing.Point(176, 51);
             this.medicationTextBox.Name = "medicationTextBox";
             this.medicationTextBox.Size = new System.Drawing.Size(100, 20);
             this.medicationTextBox.TabIndex = 1;
             // 
             // surgicalTextBox
             // 
-            this.surgicalTextBox.Location = new System.Drawing.Point(176, 97);
+            this.surgicalTextBox.Location = new System.Drawing.Point(176, 77);
             this.surgicalTextBox.Name = "surgicalTextBox";
             this.surgicalTextBox.Size = new System.Drawing.Size(100, 20);
             this.surgicalTextBox.TabIndex = 2;
             // 
             // labFeesTextBox
             // 
-            this.labFeesTextBox.Location = new System.Drawing.Point(176, 123);
+            this.labFeesTextBox.Location = new System.Drawing.Point(176, 103);
             this.labFeesTextBox.Name = "labFeesTextBox";
             this.labFeesTextBox.Size = new System.Drawing.Size(100, 20);
             this.labFeesTextBox.TabIndex = 3;
             // 
             // rehabTextBox
             // 
-            this.rehabTextBox.Location = new System.Drawing.Point(176, 149);
+            this.rehabTextBox.Location = new System.Drawing.Point(176, 129);
             this.rehabTextBox.Name = "rehabTextBox";
             this.rehabTextBox.Size = new System.Drawing.Size(100, 20);
             this.rehabTextBox.TabIndex = 4;
@@ -81,7 +87,7 @@
             // numDaysLabel
             // 
             this.numDaysLabel.AutoSize = true;
-            this.numDaysLabel.Location = new System.Drawing.Point(77, 48);
+            this.numDaysLabel.Location = new System.Drawing.Point(77, 28);
             this.numDaysLabel.Name = "numDaysLabel";
             this.numDaysLabel.Size = new System.Drawing.Size(93, 13);
             this.numDaysLabel.TabIndex = 5;
@@ -90,7 +96,7 @@
             // medChargesLabel
             // 
             this.medChargesLabel.AutoSize = true;
-            this.medChargesLabel.Location = new System.Drawing.Point(69, 74);
+            this.medChargesLabel.Location = new System.Drawing.Point(69, 54);
             this.medChargesLabel.Name = "medChargesLabel";
             this.medChargesLabel.Size = new System.Drawing.Size(101, 13);
             this.medChargesLabel.TabIndex = 6;
@@ -99,7 +105,7 @@
             // surgicalChargesLabel
             // 
             this.surgicalChargesLabel.AutoSize = true;
-            this.surgicalChargesLabel.Location = new System.Drawing.Point(83, 100);
+            this.surgicalChargesLabel.Location = new System.Drawing.Point(83, 80);
             this.surgicalChargesLabel.Name = "surgicalChargesLabel";
             this.surgicalChargesLabel.Size = new System.Drawing.Size(87, 13);
             this.surgicalChargesLabel.TabIndex = 7;
@@ -108,7 +114,7 @@
             // labFeeslabel
             // 
             this.labFeeslabel.AutoSize = true;
-            this.labFeeslabel.Location = new System.Drawing.Point(119, 126);
+            this.labFeeslabel.Location = new System.Drawing.Point(119, 106);
             this.labFeeslabel.Name = "labFeeslabel";
             this.labFeeslabel.Size = new System.Drawing.Size(51, 13);
             this.labFeeslabel.TabIndex = 8;
@@ -117,7 +123,7 @@
             // rehabLabel
             // 
             this.rehabLabel.AutoSize = true;
-            this.rehabLabel.Location = new System.Drawing.Point(47, 152);
+            this.rehabLabel.Location = new System.Drawing.Point(47, 132);
             this.rehabLabel.Name = "rehabLabel";
             this.rehabLabel.Size = new System.Drawing.Size(123, 13);
             this.rehabLabel.TabIndex = 9;
@@ -126,7 +132,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(119, 20);
+            this.label6.Location = new System.Drawing.Point(119, 9);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(114, 13);
             this.label6.TabIndex = 10;
@@ -135,24 +141,80 @@
             // totalLabel
             // 
             this.totalLabel.AutoSize = true;
-            this.totalLabel.Location = new System.Drawing.Point(139, 201);
+            this.totalLabel.Location = new System.Drawing.Point(97, 232);
             this.totalLabel.Name = "totalLabel";
-            this.totalLabel.Size = new System.Drawing.Size(31, 13);
+            this.totalLabel.Size = new System.Drawing.Size(73, 13);
             this.totalLabel.TabIndex = 11;
-            this.totalLabel.Text = "Total";
+            this.totalLabel.Text = "Total Charges";
             // 
             // totalTextBox
             // 
-            this.totalTextBox.Location = new System.Drawing.Point(176, 198);
+            this.totalTextBox.Location = new System.Drawing.Point(174, 229);
             this.totalTextBox.Name = "totalTextBox";
             this.totalTextBox.Size = new System.Drawing.Size(100, 20);
             this.totalTextBox.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Stay Charges";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Misc. Charges";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 177);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(174, 203);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // CalculateBtn
+            // 
+            this.CalculateBtn.Location = new System.Drawing.Point(321, 170);
+            this.CalculateBtn.Name = "CalculateBtn";
+            this.CalculateBtn.Size = new System.Drawing.Size(75, 23);
+            this.CalculateBtn.TabIndex = 17;
+            this.CalculateBtn.Text = "Calculate";
+            this.CalculateBtn.UseVisualStyleBackColor = true;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(321, 132);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 18;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(442, 259);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.CalculateBtn);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.totalTextBox);
             this.Controls.Add(this.totalLabel);
             this.Controls.Add(this.label6);
@@ -167,7 +229,7 @@
             this.Controls.Add(this.medicationTextBox);
             this.Controls.Add(this.numDaysTextBox);
             this.Name = "Form1";
-            this.Text = "Hospital Charges";
+            this.Text = "Hospital Visit Cost";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -188,6 +250,12 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label totalLabel;
         private System.Windows.Forms.TextBox totalTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button CalculateBtn;
+        private System.Windows.Forms.Button clearBtn;
     }
 }
 
